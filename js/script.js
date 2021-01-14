983f050666a16095ad5d594974f5af16ed0d64a5
bharathvc007 <71154112+bharathvc007@users.noreply.github.com>
2021-01-14 19:29:41 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+ $(function () {
+      $('#WAButton').floatingWhatsApp({
+          phone: '916360859810',
+          headerTitle: 'Chat with us on WhatsApp!',
+          popupMessage: 'Hello, Welcome to Bitely how can we help you?',
+          showPopup: true,
+          buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />',
+          headerColor: '#e99838', //Custom header color
+          backgroundColor: '#e99838', //Custom background button color
+          position: "right"
+      });
+  });
